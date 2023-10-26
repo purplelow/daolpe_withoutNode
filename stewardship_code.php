3da7de361b899b90162be98b8d61f6cac2a88b2b
JSCreation <purplelow1@gmail.com>
2023-10-27 08:01:35 +0900

    Feat: regacy code add

---
+++ stewardship_code.php
@@ -0,0 +1,359 @@
+<!DOCTYPE html>
+<html lang="ko">
+
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<meta name="format-detection" content="telephone=no, address=no, email=no" />
+	<!--모든 전화링크 자동해제-->
+	<!--favicon-->
+	<link rel="shortcut icon" href="img/favicon/favicon.ico" type="image/x-icon" />
+	<link rel="icon" href="img/favicon/favicon.ico" type="image/x-icon" />
+
+
+	<title>DAOL Private Equity</title>
+	<!--CSS-->
+	<link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/gh/moonspam/NanumSquare@1.0/nanumsquare.css">
+	<link rel="stylesheet" href="css/reset.css" type="text/css" />
+	<!--모든브라우저 호환성을위한 초기화 CSS 파일-->
+	<link rel="stylesheet" href="css/common.css" type="text/css" />
+	<!--공통 CSS파일-->
+	<link rel="stylesheet" href="css/style.css" type="text/css" /> <!-- 메인 CSS파일 -->
+	<link rel="stylesheet" href="css/jquery-ui.min.css" type="text/css" /> <!-- UI CSS 파일-->
+	<!--script-->
+	<script src="js/modernizr-custom.js"></script> <!-- 브라우저 호환성 체크 -->
+	<script src="js/jquery_1.12.4.js"></script> <!-- 제이쿼리 사용을위한 js파일-->
+	<script src="js/jquery-ui.min.js"></script>
+	<!--제이쿼리 ui js파일-->
+	<script src="js/main.js"></script> <!-- 메인 js파일-->
+
+</head>
+<!--[if lt IE 9]> 
+    <script src="js/html5shiv.min.js"></script> 
+<![endif]-->
+
+<body>
+	<div id="wrap">
+		<header id="header">
+			<div class="row2">
+				<div class="header ">
+
+					<div class="hed_fixed">
+						<div class="header_logo clearfix">
+							<h1 class="logo">
+								<a href="index.php">
+									<img src="img/daolpe_logo.png" alt="DAOL Private Equity">	
+								</a>
+							</h1>
+						</div>
+					</div>
+
+					<p class="header_txt Mr_pro">STEWARDSHIP CODE</p>
+					<h2 class="header_tit">수탁자책임정책</h2>
+				</div>
+			</div>
+		</header>
+		<!-- //header -->
+		<section id="content">
+			<div class="row">
+				<div class="container">
+					<div class="content">
+						<p class="content_toptxt">
+							다올프라이빗에쿼티 주식회사(이하 “다올 PE”라 합니다)는 「자본시장과 금융투자업에 관한 법률」(“자본시장법”)에서 규정한 기관전용 사모집합투자기구(이하 “PEF”라 합니다)를 설립하여 운용하는 자산운용자(asset manager)로서 투자대상회사의 중장기적인 가치 향상과 지속가능한 성장을 추구함으로써 자본시장과 경제전반의 내실 있는 성장과 발전에 기여하고 수익자의 중장기적 이익을 도모하기 위하여 2023년 3월 7일부로 다올 PE Stewardship Code를 수립하여 시행합니다.
+						</p>
+						<div class="prin_wrap">
+							<dl class="prin_area prin_desc1">
+								<dt class="prin_tit">원칙 1.</dt>
+								<dd class="prin_txt1">
+									기관투자자는 고객, 수익자 등 타인 자산을 관리ㆍ운영하는 수탁자로서 책임을 충실히 이행하기 위한 명확한 정책을 마련해 공개해야 한다.
+								</dd>
+								<dd class="prin_txt2">
+									<ul class="prin_inner">
+										<li class="prin_list_txt prin_list1">다올 PE는 자본시장법 제249조의15에 따라 금융위원회에 등록한 PEF의 업무집행사원으로서 선량한 관리자의 주의의무를 다하기 위하여 경영진 책임 하에 수탁자 책임 정책을 수립하고 공개하며, 해당 정책의 적정여부를 연1회 이상 주기적으로 검토합니다.</li>
+										<li class="prin_list_txt prin_list2">다올 PE는 PEF 관련 모든 제반 법령을 준수하고, PEF정관 및 수익자와의 합의사항을 충실히 이행합니다.</li>
+										<li class="prin_list_txt prin_list3">수탁자 책임 정책을 효율적으로 관리하기 위하여 회사 내에 스튜어드십코드 책임자를 정하여 정책의 이행 관리를 철저히 합니다.</li>
+										<li class="prin_list_txt prin_list4">수탁자 책임 정책은 PEF 설립에서 청산에 이르기 까지 전 과정에 걸쳐 적용됩니다.</li>
+									</ul>
+								</dd>
+							</dl>
+
+							<dl class="prin_area prin_desc3">
+								<dt class="prin_tit">원칙 2.</dt>
+								<dd class="prin_txt1">
+									기관투자자는 수탁자로서 책임을 이행하는 과정에서 실제 직면하거나 직면할 가능성이 있는 이해상충 문제를 어떻게 해결할지에 관해 효과적이고 명확한 정책을 마련하고 내용을 공개해야 한다.
+								</dd>
+								<dd class="prin_txt2">
+									<ul class="prin_inner">
+										<li class="prin_list_txt prin_list1">다올 PE는 당사의 이익에 우선하여 수익자의 이익을 최우선과제로 여기고 있으며, PEF 운용에 있어 발생할 수 있는 이해상충상황을 적절하게 관리하기 위하여 “이해상충방지정책”을 수립하여 운영합니다.</li>
+										<li class="prin_list_txt prin_list2">주주활동 과정에서 미공개 중요 정보를 취득하는 경우에도 해당 정보를 이용하여 다올 PE의 거래상 이득을 취하려는 시도를 하지 않습니다. </li>
+										<li class="prin_list_txt prin_list3">다올 PE 내부통제기준, 윤리규정, PEF정관 및 업무집행사원행위준칙을 통하여 이해상충방지에 관한 사항을 규정함으로써 경영활동 전반에 있어 유기적으로 이해상충을 예방합니다. </li>
+									</ul>
+								</dd>
+							</dl>
+
+							<dl class="prin_area prin_desc4">
+								<dt class="prin_tit">원칙 3.</dt>
+								<dd class="prin_txt1">
+									기관투자자는 투자대상회사의 중장기적인 가치를 제고하여 투자자산의 가치를 보존하고 높일 수 있도록 투자대상회사를 주기적으로 점검해야 한다.
+								</dd>
+								<dd class="prin_txt2">
+									<ul class="prin_inner">
+										<li class="prin_list_txt prin_list1">다올 PE는 투자 실행 전에 투자대상회사를 현장 실사하여 재무제표 작성의 적정여부, 부실 가능성
+											여부, 기타 기업의 내재된 리스크 등을 점검하며, 점검결과를 투자심의위원회에 보고하여 투자의사결정의 신뢰성을 기합니다
+										</li>
+										<li class="prin_list_txt prin_list2">투자 계약 체결 단계에서 이사 선임권을 확보하여 투자대상회사의 경영활동을 감독하고, 이사회를 통해 적극적으로 기업가치 향상을 위한 의견을 개진함을 원칙으로 합니다 </li>
+										<li class="prin_list_txt prin_list3">투자 집행 이후 다올 PE는 PEF 업무집행사원으로서 연초 투자대상회사의 사업계획을 점검하고, 주주총회 제출 전 감사보고서 및 영업보고서를 점검합니다. </li>
+										<li class="prin_list_txt prin_list4">다올 PE는 투자대상회사의 경영활동 전반을 체크하고, 중요 현안을 파악하여 대처합니다.</li>
+									</ul>
+								</dd>
+							</dl>
+
+							<dl class="prin_area prin_desc5">
+								<dt class="prin_tit">원칙 4.</dt>
+								<dd class="prin_txt1">
+									기관투자자는 투자대상회사와의 공감대 형성을 지향하되, 필요한 경우 수탁자 책임 이행을 위한 활동 전개 시기와 절차, 방법에 관한 내부지침을 마련해야 한다.
+								</dd>
+								<dd class="prin_txt2">
+									<ul class="prin_inner">
+										<li class="prin_list_txt prin_list1">다올 PE는 투자대상회사의 대주주 등과 주주간 계약을 체결하여 투자대상회사의 기업가치 향상을 위한 공동 대응방안을 모색하고, 주주 및 투자대상회사 상호간의 이익을 극대화하기 위하여 노력합니다.
+										</li>
+										<li class="prin_list_txt prin_list2">다올 PE는 투자대상회사와의 효율적인 커뮤니케이션 및 관리를 위하여 투자관리규정을 제정하여 운영합니다. 동규정을 통해 상황별 투자대상회사 관리방안 및 파견임직원의 지위와 책임 등을 명시하고, 다올 PE 임직원들로 하여금 이를 준수하도록 하여 수탁자로서의 책임 이행활동을 구체적으로 전개합니다. </li>
+									</ul>
+								</dd>
+							</dl>
+
+							<dl class="prin_area prin_desc6">
+								<dt class="prin_tit">원칙 5.</dt>
+								<dd class="prin_txt1">
+									기관투자자는 충실한 의결권 행사를 위한 지침ㆍ절차ㆍ세부기준을 포함한 의결권 정책을 마련해 공개해야 하며, 의결권 행사의 적정성을 파악할 수 있도록 의결권 행사의 구체적인 내용과 그 사유를 함께 공개해야 한다.
+								</dd>
+								<dd class="prin_txt2">
+									<ul class="prin_inner">
+										<li class="prin_list_txt prin_list1">다올 PE는 PEF에서 보유하고 있는 주식 전부에 대해 의결권을 행사하며, 사전에 주주총회 안건을 면밀하게 검토합니다.
+										</li>
+										<li class="prin_list_txt prin_list2">의결권 행사는 다올 PE의 책임과 판단 하에 이루어지며, 경영진의 승인을 득하여 행사합니다.</li>
+										<li class="prin_list_txt prin_list3">다올 PE는 투자대상회사의 중장기적인 가치 증진에 기여할 수 있도록 “의결권행사 가이드라인”을 마련하여 운영합니다. </li>
+										<li class="prin_list_txt prin_list4">의결권행사의 구체적인 내용과 그 사유는 PEF 사원총회 또는 자문위원회에서 공개하며, 불특정 다수의 일반에게는 공개하지 아니합니다.</li>
+									</ul>
+								</dd>
+							</dl>
+
+							<dl class="prin_area prin_desc7">
+								<dt class="prin_tit">원칙 6.</dt>
+								<dd class="prin_txt1">
+									기관투자자는 의결권 행사와 수탁자 책임 이행 활동에 관해 고객과 수익자에게 주기적으로 보고해야 한다.
+								</dd>
+								<dd class="prin_txt2">
+									<ul class="prin_inner">
+										<li class="prin_list_txt prin_list1">다올 PE는 PEF 영업보고서를 통하여 매 반기 수익자에게 의결권 행사내역 및 수탁자 책임 이행 활동내역을 공개합니다.
+										</li>
+										<li class="prin_list_txt prin_list2">다올 PE는 PEF별 Contact Point를 지정하여 수익자와의 지속적인 커뮤니케이션에 최선을 다합니다.</li>
+									</ul>
+								</dd>
+							</dl>
+
+							<dl class="prin_area prin_desc2">
+								<dt class="prin_tit">원칙 7.</dt>
+								<dd class="prin_txt1">
+									기관투자자는 수탁자 책임의 적극적이고 효과적인 이행을 위해 필요한 역량과 전문성을 갖추어야 한다.
+								</dd>
+								<dd class="prin_txt2">
+									<ul class="prin_inner">
+										<li class="prin_list_txt prin_list1">다올 PE는 PEF의 설립 및 운용, M&A, 투자의사결정 및 투자대상회사 기업가치 개선 등에 있어 업계 최고 수준의 전문가로 구성되어 있으며, 국내외 유수의 외부 전문가 집단과 유기적인 공조·협력 하에 업무를 수행합니다. </li>
+										<li class="prin_list_txt prin_list2">다올 PE는 임직원의 전문성 함양을 위한 노력을 지속적으로 장려하고 적극 지원함과 더불어 필요
+											시 수익자 및 그 구성원의 전문성 습득 활동을 지원하여 수익자와의 동반자적 전문 역량 향상을
+											추구합니다.
+										</li>
+									</ul>
+								</dd>
+								<!--pc 일시 -->
+								<dd class="prin_txt3">
+									<span class="prin_underline"></span>
+									<p class="prin_txt3_tit">Stewardship Code 책임자 및 담당자</p>
+									<table class="prin_table">
+										<!-- <caption class="ir_so">구분, 성명, 부서, 직책(직위), 연락처, 이메일</caption> -->
+										<colgroup>
+											<col width="15%">
+											<col width="15%">
+											<col width="20%">
+											<col width="20%">
+											<col width="30%">
+										</colgroup>
+										<thead>
+											<tr>
+												<th>구분</th>
+												<th>성명</th>
+												<th>부서</th>
+												<th>직위</th>
+												<th>이메일 연락처</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td class="td_tit">책임자</td>
+												<td>윤정주</td>
+												<td>경영기획</td>
+												<td>이사</td>
+												<td>jungjoo.yun@daolpe.com</td>
+											</tr>
+											<tr>
+                                                <td class="td_tit">담당자</td>
+                                                <td>신지원</td>
+                                                <td>경영기획</td>
+                                                <td>과장</td>
+                                                <td>jiwon.shin@daolpe.com</td>
+                                            </tr>
+											<!-- <tr>
+												<td class="td_tit">담당자</td>
+												<td class="br_b">김제균</td>
+												<td class="br_b">투자본부</td>
+												<td class="br_b">투자담당 (부장)</td>
+												<td class="br_b">02-2184-4175</td>
+												<td class="br_b">jekyun.kim@daolpe.com</td>
+											</tr> -->
+										</tbody>
+									</table>
+								</dd>
+								
+								<!-- 모바일일시 -->
+								<dd class="prin_txt4">
+									<span class="prin_underline"></span>
+									<p class="prin_txt4_tit">Stewardship Code 책임자 및 담당자</p>
+									<table class="prin_table2">
+										<!-- <caption>구분, 성명, 부서, 직책(직위), 연락처, 이메일</caption> -->
+										<colgroup>
+											<col style="width:15%">
+											<col style="width:40%">
+											<col style="width:45%">
+										</colgroup>
+										<thead>
+											<tr>
+												<th>구분<br />성명</th>
+												<th>부서<br />직위</th>
+												<th>연락처<br />이메일</th>
+											</tr>
+										</thead>
+										<tbody>
+											<tr>
+												<td><span class="td_tit">책임자</span><br />윤정주</td>
+												<td>경영기획<br />이사</td>
+												<td>jungjoo.yun@daolpe.com</td>
+											</tr>
+											<tr>
+                                                <td><span class="td_tit">담당자</span><br />신지원</td>
+                                                <td>경영기획<br />과장</td>
+                                                <td>jiwon.shin@daolpe.com</td>
+                                            </tr>
+											<!-- <tr>
+												<td><span class="td_tit">담당자</span><br />윤정주</td>
+												<td>경영기획<br />Compliance 담당 (부장)</td>
+												<td>02-2184-4175<br/>jekyun.kim@daolpe.com</td>
+											</tr> -->
+										</tbody>
+									</table>
+								</dd>
+
+							</dl>
+						</div>
+						<div class="content_donw_wrap">
+							<ul class="content_down clearfix">
+								<li class="down_guide"><a href="pdf/guideline.pdf" download="의결권행사-가이드라인.pdf">의결권행사 가이드라인</a></li>
+								<li class="down_policy"><a href="pdf/prevention.pdf" download="이해상충방지정책.pdf">이해상충방지정책</a></li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+		<!-- content -->
+		<footer id="footer">
+			<div class="row">
+				<!-- <div class="container"> -->
+				<div class="footer">
+					<div class="footer_header clearfix">
+						<div class="footer_logo">
+							<img src="img/daolpe_logo.png" alt="DAOL Private Equity">
+						</div>
+						<div class="footer_familysite">
+							<div class="family_box">
+								<a href="javascript:;" class="family_main">Family Site</a>
+								<ul class="family_sub">
+									<li><a href="http://www.daolsecurities.com/" target="_blank">다올투자증권</a></li>
+									<li><a href="https://www.daolsb.com" target="_blank">다올저축은행</a></li>
+									<li><a href="http://www.daolfund.com/" target="_blank">다올자산운용</a></li>
+									<li><a href="http://www.daolinvestment.com/" target="_blank">다올인베스트먼트</a></li>
+									<li><a href="http://www.daolcredit.com/" target="_blank">다올신용정보</a></li>
+									<li><a href="https://www.daolventures.com/" target="_blank">DAOL Ventures</a></li>
+									<li><a href="https://www.ktbst.co.th/" target="_blank">DAOL THAILAND</a></li>
+								</ul>
+							</div>
+						</div>
+					</div>
+					<div class="footer_top_txt">
+						<ul>
+							<li class="Mr_pro">30Fl, POST Tower, 60, Yeouinaru-ro, Yeongdeungpo-gu, Seoul, Korea <span> <strong>T</strong>. +82 2 2184 4150</span><span> <strong>F</strong>. +82 2 2184 4192</span></li>
+							<li class="Mr_pro">COPYRIGHT© DAOL Private Equity ALL RIGHTS RESERVED.</li>
+						</ul>
+					</div>
+					<div class="footer_bot_txt">
+						<ul>
+							<li class="Mr_pro">This website is not intended to constitute any offer to sell or the invitation or solicitation of an offer to buy and product or service offered by DAOL Private Equity Co., Ltd. or any affiliate of such company ("다올 PE") and must not be relied upon as such. DAOL PE is not responsible for damages of any kind arising out of use, reference to, or reliance on any information contained within this site. You must not use our site in any way which is unlawful, illegal, fraudulent, or harmful, or in connection with any unlawful, illegal, fraudulent, or harmful purpose or activity. We accept no responsibility for any loss or harm incurred.
+							</li>
+						</ul>
+					</div>
+				</div>
+				<!-- </div> -->
+			</div>
+		</footer>
+	</div>
+	<!-- //wrap -->
+	<a href="javascript:;" class="fixed"><span class="ir_pm">맨위로</span></a>
+	<!-- fixed ico -->
+
+	<!-- topdixed -->
+	<div class="top_header">
+		<ul class="clearfix">
+			<li class="top_logo"><a href="index.php"><span class="ir_so">픽스트로고</span></a></li>
+			<!-- <li class="top_site"><a href="stewardship_code.php">STEWARDSHIP CODE</a></li> -->
+		</ul>
+
+	</div>
+
+	<script>
+		$(window).scroll(function () {
+			var scroll_Value2 = $(document).scrollTop();
+			if (scroll_Value2 >= 50) {
+				$('.fixed').stop().animate({
+					'opacity': '1'
+				}, 100);
+			} else {
+				$('.fixed').stop().animate({
+					'opacity': '0'
+				}, 100);
+			}
+
+			if (scroll_Value2 >= 1) {
+				$('.hed_fixed').addClass('on');
+			} else {
+				$('.hed_fixed').removeClass('on');
+			}
+			if (scroll_Value2 >= 80) {
+				$('.hed_fixed').addClass('bg');
+			} else {
+				$('.hed_fixed').removeClass('bg');
+			}
+
+		})
+
+		$('.fixed').click(function () {
+			$('html, body').animate({
+				scrollTop: 0
+			}, 400);
+			return false;
+		})
+	</script>
+
+</body>
+
+</html>
